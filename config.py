58912adbeea65265edfdb7b66aa8db2f214ae424
wangyu <wangyucon@163.com>
2021-04-15 17:47:04 +0800

    导入配置文件

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+PRO_PATH = os.path.dirname(os.path.abspath(__file__))
+
+class RunConfig:
+    """
+    运行测试配置
+    """
+    # 运行测试用例的目录
+    case_path = os.path.join(PRO_PATH,'testcase')
+
+    # 平台名称
+    platformName = 'Android'
+
+    # 平台版本
+    platformVersion = '7'
+
+    # 设备名称
+    deviceName = 'HONOR 20 Lite'
+
+    # 果皮日记包名
+    appPackage = 'com.dongxiangtech.peeldiary'
+
+    # 果皮日记首页Activity
+    appActivity = '.main.MainActivity'
+
+    # 真机驱动（不需要修改）
+    driver = None
+
+    # 失败重跑次数
+    rerun = "1"
+
+    # 当达到最大失败数，停止执行
+    max_fail = "5"
--- conftest.py
+++ conftest.py
@@ -1,19 +1,27 @@
 import pytest
-
+import os
 from appium import webdriver
+from config import RunConfig
 
+# 项目报告目录配置
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+REPORT_DIR = BASE_DIR + "/testreport/"
 
 @pytest.fixture(scope='class')
 def init_peel():
+
+    global driver
     desired_caps = {}
-    desired_caps['platformName'] = 'Android'
-    # 真机系统版本
-    # desired_caps['platformVersion'] = '10'
-    desired_caps['platformVersion'] = '7'
-    desired_caps['deviceName'] = 'HONOR 20 Lite'
-    desired_caps['appPackage'] = 'com.dongxiangtech.peeldiary'
-    # 真机启动Activity
-    desired_caps['appActivity'] = '.main.MainActivity'
+    desired_caps['platformName'] = RunConfig.platformName
+    desired_caps['platformVersion'] = RunConfig.platformVersion
+    desired_caps['deviceName'] = RunConfig.deviceName
+    desired_caps['appPackage'] = RunConfig.appPackage
+    desired_caps['appActivity'] = RunConfig.appActivity
     driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
+
+    RunConfig.driver = driver
+
     yield driver
+
     driver.quit()
+    print("test end!")
